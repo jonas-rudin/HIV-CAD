5cb2c268b6e436c991d9f7289863c0ebcdd5bd72
jonas-rudin <98404434+jonas-rudin@users.noreply.github.com>
2022-07-26 11:01:45 +0200

    feat: add 454 error rate to create reads

--- create_data.py
+++ create_data.py
@@ -1,5 +1,5 @@
 from os.path import exists
-from random import randint, shuffle
+from random import randint, shuffle, uniform, choice
 
 import numpy as np
 import tensorflow as tf
@@ -10,8 +10,25 @@ import one_hot
 with open("./config.yml", "r") as ymlfile:
     config = yaml.safe_load(ymlfile)
 
+bases = ['A', 'C', 'G', 'T']
+
 
 # TODO add sequencing error (randomly remove or change base) (according to 454 error)
+# add in an error to the read error rate: 0.49% per base
+# ignoring difference in homopolymeric and non-homopolymeric regions
+def add_error(read):
+    i = 0
+    while i < (len(read)):
+        if uniform(0, 1) < 1:  # 0.0049:
+            replacement_base = choice(bases)
+            if read[i] == replacement_base:
+                read = read[:i] + read[i + 1:]
+                continue
+            else:
+                read = read[:i] + replacement_base + read[i + 1:]
+            i += 1
+    return read
+
 
 # cut original haplotype in reads of certain length, creating at least amount reads
 def cut_into_reads(rna, length, amount):
@@ -20,11 +37,12 @@ def cut_into_reads(rna, length, amount):
         first_index = 0
         last_index = randint(1, length - 1)
         prov_rna_reads = []
+        # go through rna and cut it into reads
         while last_index < len(rna):
-            prov_rna_reads.append(rna[first_index:last_index])
+            prov_rna_reads.append(add_error(rna[first_index:last_index]))
             first_index = last_index
             last_index += length + int(np.random.normal(0, length * 0.2, 1)[0])
-        prov_rna_reads.append(rna[first_index:len(rna)])
+        prov_rna_reads.append(add_error(rna[first_index:len(rna)]))
         rna_reads.extend(prov_rna_reads)
     return rna_reads
 
@@ -47,14 +65,13 @@ def create_reads():
     reads = []
     for _ in range(number_of_strains):
         print("Creating reads from strain:", (f.readline())[1:])
-        rna = f.readline()[:-1]
         # create reads
-        reads.extend(cut_into_reads(rna, read_length, min_number_of_reads_per_strain))
+        reads.extend(cut_into_reads(f.readline()[:-1], read_length, min_number_of_reads_per_strain))
     shuffle(reads)
     # one hot encoding, convert list to tensor
     # and add dimension (needed for conv2D in tensorflow shape(x,y) -> shape(x,y,1))
     one_hot_encoded_reads = tf.expand_dims(
-        tf.convert_to_tensor([one_hot.encode_read(read, len(rna)) for read in reads]), axis=3)
+        tf.convert_to_tensor([one_hot.encode_read(read, config["max_created_read_length"]) for read in reads]), axis=3)
 
     # save
     if config['save']:
--- main.py
+++ main.py
@@ -17,17 +17,18 @@ if __name__ == '__main__':
     print("Python version: {}.{}.{}".format(sys.version_info[0], sys.version_info[1], sys.version_info[2]))
     print("Tensorflow version: {}".format(tf.__version__))
     if config["real_data"]:
-        print(f"Working with {ColorCoding.OKCYAN}454{ColorCoding.ENDC} reads")
+        print(f"Working with {ColorCoding.OKGREEN}454{ColorCoding.ENDC} reads")
         data = one_hot.encode()
     else:
         print(
-            f"Working with {ColorCoding.OKCYAN}created{ColorCoding.ENDC} reads")
+            f"Working with {ColorCoding.OKBLUE}created{ColorCoding.ENDC} reads")
         data = create_data.create_reads()
-    batch_size = int(np.ceil(data.shape[0] / 20))
+    batch_size = int(np.ceil(data.shape[0] / 200))
     autoencoder = Autoencoder(data.shape[1:])
 
     autoencoder.compile(optimizer="adam", loss=tf.keras.losses.MeanSquaredError())
     autoencoder.build(input_shape=(data.shape))
+    print(data.shape)
     autoencoder.fit(x=data, y=data,
                     epochs=2,
                     batch_size=batch_size,
