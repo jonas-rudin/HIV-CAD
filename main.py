working but wrong dimensions

main.py
@@ -16,9 +16,10 @@ import performance
 from helpers.IO import load_tensor_file
 from helpers.colors_coding import ColorCoding
 from helpers.config import get_config
-from models.autoencoder import get_autoencoder_key_points_with_pooling, get_autoencoder_key_points
+from models.autoencoder import get_CAECseq
 from models.layers.cluster import ClusteringLayer
 
+# tf.compat.v1.disable_eager_execution()
 # in bytes
 config = get_config()
 data = config['data']
@@ -32,11 +33,47 @@ data = config['data']
 # visible_devices = tf.config.get_visible_devices()
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
+
 # for device in visible_devices:
 #     assert device.device_type != 'GPU'
 # distribution_strategy = tf.distribute.MirroredStrategy()
 
+
+def test():
+    one_hot_encoded_reads = np.array([[[[1], [0], [0], [0]], [[0], [0], [1], [0]], [[1], [0], [0], [0]]],
+                                      [[[0], [1], [0], [0]], [[0], [0], [1], [0]], [[0], [0], [0], [1]]],
+                                      [[[0], [1], [0], [0]], [[0], [0], [1], [0]], [[0], [0], [0], [1]]],
+                                      [[[0], [1], [0], [0]], [[0], [0], [1], [0]], [[0], [0], [0], [1]]],
+                                      [[[1], [0], [0], [0]], [[0], [1], [0], [0]], [[1], [0], [0], [0]]]])
+    # print(one_hot_encoded_reads)
+    predicted_clusters = np.array([0, 1, 1, 1, 1])
+    # print(predicted_clusters)
+    n_clusters = 2
+    consensus_sequences = majority_voting.align_reads_per_cluster(one_hot_encoded_reads,
+                                                                  predicted_clusters, n_clusters)
+
+    predicted_clusters = majority_voting.assign_reads_to_best_fitting_consensus_sequence(one_hot_encoded_reads.shape[0],
+                                                                                         n_clusters,
+                                                                                         consensus_sequences,
+                                                                                         one_hot_encoded_reads)
+    print(predicted_clusters)
+    # print('mec:', performance.minimum_error_correction(one_hot_encoded_reads, consensus_sequences))
+    # print('result:', consensus_sequences)
+
+
 if __name__ == '__main__':
+    # test()
+    # exit(-1)
+    # with open("CPR", "rb") as fp:
+    #     old_CPR = pickle.load(fp)
+    # print(old_CPR)
+    # if data == 'created':
+    #     reference_sequences = load_tensor_file(
+    #         config[data]['aligned_ref_path'] + '_' + str(config[data]['n_clusters']))
+    # else:
+    #     reference_sequences = load_tensor_file(config[data]['aligned_ref_path'])
+    # print('\nreference_sequences:\n', one_hot.decode(reference_sequences, True))
+    # exit(-1)
     print(tf.keras.backend.floatx())
     # TODO print config and info
     print('Python version: {}.{}.{}'.format(sys.version_info[0], sys.version_info[1], sys.version_info[2]))
@@ -45,7 +82,7 @@ if __name__ == '__main__':
     print(f'Working with {ColorCoding.OKGREEN}{data}{ColorCoding.ENDC} reads')
     number_of_files = one_hot.encode_sam()
 
-    n_clusters = config['n_clusters']
+    n_clusters = config[data]['n_clusters']
     verbose = config['verbose']
 
     # if data == 'illumina':
@@ -67,7 +104,14 @@ if __name__ == '__main__':
         with tf.device('/cpu:0'):
             one_hot_encoded_reads = load_tensor_file(config[data]['one_hot_path'])
             shape = one_hot_encoded_reads.shape
-
+    # for i in range(shape[0]):
+    #     print('0', one_hot_encoded_reads[i][0])
+    #     print(np.sum(one_hot_encoded_reads[i][0]))
+    #     print('1', one_hot_encoded_reads[i][1])
+    #     if np.array_equal(one_hot_encoded_reads[i][1], np.array([[0], [0], [0], [0]])) and np.sum(
+    #             one_hot_encoded_reads[i][0] != 0):
+    #         print('found')
+    # exit(-1)
     with tf.device('/cpu:0'):
         dataset = tf.data.Dataset.from_tensor_slices((one_hot_encoded_reads, one_hot_encoded_reads))
         dataset = dataset.shuffle(10).batch(config[data]['batch_size'])  # .prefetch(2)
@@ -84,24 +128,25 @@ if __name__ == '__main__':
     pooling = ' pooling' if config[data]['pooling'] else 'out pooling';
     print(f'{ColorCoding.OKGREEN}Model build with{pooling}{ColorCoding.ENDC}')
     # with distribution_strategy.scope():
-    if config[data]['pooling']:
-        model_input, latent_space, decoder_output = get_autoencoder_key_points_with_pooling(
-            shape[1:])
-    else:
-        model_input, latent_space, decoder_output = get_autoencoder_key_points(
-            shape[1:])
+    print(shape[1:])
+    model_input, latent_space, decoder_output = get_CAECseq(shape[1:])
+    # if config[data]['pooling']:
+    #     model_input, latent_space, decoder_output = get_autoencoder_key_points_with_pooling(shape[1:])
+    # else:
+    #     model_input, latent_space, decoder_output = get_autoencoder_key_points(shape[1:])
+
     print(f'{ColorCoding.OKGREEN}building autoencoder{ColorCoding.ENDC}')
 
     autoencoder = Model(inputs=model_input, outputs=decoder_output, name='autoencoder')
     autoencoder.compile(optimizer='adam', loss=MeanSquaredError())
-    autoencoder.optimizer.lr.assign(0.001)
+    # autoencoder.optimizer.lr.assign(0.001)
     # TODO delete following two lines
     autoencoder.build(input_shape=shape)
     autoencoder.summary()
 
     encoder = Model(inputs=model_input, outputs=latent_space, name='encoder')
     encoder.compile(optimizer='adam', loss=MeanSquaredError())
-    encoder.optimizer.lr.assign(0.001)
+    # encoder.optimizer.lr.assign(0.001)
     encoder.summary()
 
     print(f'{ColorCoding.OKGREEN}building CAE{ColorCoding.ENDC}')
@@ -111,7 +156,7 @@ if __name__ == '__main__':
     clustering_model = Model(inputs=model_input,
                              outputs=clustering_layer)
     encoder.compile(optimizer='adam', loss=KLD)
-    encoder.optimizer.lr.assign(0.001)
+    # encoder.optimizer.lr.assign(0.001)
 
     lam = 0.1
     cae_model = Model(inputs=model_input,
@@ -119,7 +164,7 @@ if __name__ == '__main__':
     cae_model.compile(loss=[MeanSquaredError(), KLD],
                       loss_weights=[1 - lam, lam],
                       optimizer='adam')
-    cae_model.optimizer.lr.assign(0.001)
+    # cae_model.optimizer.lr.assign(0.001)
 
     print('reads tensor shape:', shape)
     print('number of reads:', shape[0])
@@ -134,31 +179,21 @@ if __name__ == '__main__':
     else:
         print(f'{ColorCoding.OKGREEN}Training Autoencoder{ColorCoding.ENDC}')
         autoencoder.fit(x=dataset,
-                        epochs=100,
+                        epochs=50,
                         verbose=config['verbose'])
-
-        # autoencoder.fit(x=one_hot_encoded_reads, y=one_hot_encoded_reads,
-        #                 epochs=100,
-        #                 batch_size=batch_size,
-        #                 shuffle=True,
-        #                 verbose=config['verbose'])
 
         print(f'{ColorCoding.OKGREEN}Saving Weights{ColorCoding.ENDC}')
         autoencoder.save_weights(config[data]['weights_path'])
 
     print(f'{ColorCoding.OKGREEN}Build Clustering Model{ColorCoding.ENDC}')
 
-    # train k-means
+    # dataset = tf.data.Dataset.from_tensor_slices((one_hot_encoded_reads, one_hot_encoded_reads))
+    # dataset = dataset.batch(config[data]['batch_size'])
+    prediction_for_kmeans_training = encoder.predict(x=one_hot_encoded_reads, batch_size=config[data]['batch_size'],
+                                                     verbose=verbose)
+    # prediction_for_kmeans_training = encoder.predict(dataset, verbose=verbose)
 
-    # NEW outside not inside loop
-    # todo uncomment
-    prediction_for_kmeans_training = encoder.predict(dataset, verbose=1)  # TODO change to verbose
-    # max_clusters = config['max_clusters']
-    # for n_clusters in range(1, max_clusters):
-    n_clusters = config['n_clusters']
-    # one_hot_encoded_reads = load_tensor_file(config[data]['one_hot_path'])
-
-    kmeans_rep = 10  # TODO set to 10
+    kmeans_rep = 10
     best_mec_result = 0
     print('n_clusters:', n_clusters)
     print(f'{ColorCoding.OKGREEN}Initialize KMeans{ColorCoding.ENDC} for ' + str(n_clusters) + ' clusters (' + str(
@@ -166,7 +201,7 @@ if __name__ == '__main__':
     # TODO uncomment -->
     for i in range(kmeans_rep):
         print('kmeans_rep:', i)
-        kmeans = KMeans(n_clusters=n_clusters, n_init=30, verbose=0)  # TODO change to verbose
+        kmeans = KMeans(n_clusters=n_clusters, n_init=30, verbose=0)
         predicted_clusters = kmeans.fit_predict(prediction_for_kmeans_training)
         consensus_sequences = majority_voting.align_reads_per_cluster(one_hot_encoded_reads,
                                                                       predicted_clusters, n_clusters)
@@ -177,9 +212,9 @@ if __name__ == '__main__':
             best_mec_result = new_mec_result
             best_consensus_sequences = consensus_sequences
     # TODO till here <--
-    # print('Best MEC after cluster initialisation:', best_mec_result)
-    # _, _, cpr = performance.correct_phasing_rate(best_consensus_sequences)
-    # print('CRP after centroids:', cpr)
+    print('Best MEC after cluster initialisation:', best_mec_result)
+    _, _, cpr = performance.correct_phasing_rate(best_consensus_sequences)
+    print('CRP after centroids:', cpr)
 
     # TODO delete incl. file
     with open(config[data]['centroids_name'], "wb") as fp:
@@ -188,17 +223,13 @@ if __name__ == '__main__':
     #     centroids = pickle.load(fp)
 
     cae_model.get_layer(name='clustering').set_weights([centroids])
-    # cae_model.build(input_shape=one_hot_encoded_reads.shape)
-    # cae_model.summary()
-
-    mec_results = []
 
     old_mec_result = 0
     for epoch in range(20):  # TODO set to 2000
         print('epoch', epoch)
         # TODO try if possible
-        clustering_output = clustering_model.predict(dataset, verbose=config['verbose'])
-        # _, clustering_output = cae_model.predict(dataset, verbose=config['verbose'])
+        clustering_output = clustering_model.predict(x=one_hot_encoded_reads, verbose=config['verbose'])
+        # _, clustering_output = cae_model.predict(x=one_hot_encoded_reads, verbose=config['verbose'])
         # TODO delete incl. file
         with open("clustering_output", "wb") as fp:
             pickle.dump(clustering_output, fp)
@@ -214,14 +245,13 @@ if __name__ == '__main__':
 
         consensus_sequences = majority_voting.align_reads_per_cluster(one_hot_encoded_reads,
                                                                       predicted_clusters, n_clusters)
-        decoded_sequences = one_hot.decode(consensus_sequences)
+        # decoded_sequences = one_hot.decode(consensus_sequences)
 
         new_mec_result = performance.minimum_error_correction(one_hot_encoded_reads, consensus_sequences)
         print('new mec result', new_mec_result)
 
         # build clustering output per read depending on hamming distance at non-zero places
-        predicted_clusters = majority_voting.assign_reads_to_best_fitting_consensus_sequence(shape[0],
-                                                                                             n_clusters,
+        predicted_clusters = majority_voting.assign_reads_to_best_fitting_consensus_sequence(n_clusters,
                                                                                              consensus_sequences,
                                                                                              one_hot_encoded_reads)
         predicted_clusters_tensor = tf.convert_to_tensor(predicted_clusters, dtype=tf.int8)
@@ -230,36 +260,63 @@ if __name__ == '__main__':
             break
 
         old_mec_result = new_mec_result
+        # batch_size = config[data]['batch_size']
+        # n_read = shape[0]
+        # index = 0
+        #
+        # # for i in range(int(np.ceil(n_read / batch_size))):
+        # #     if (index + 1) * batch_size > n_read:
+        # #         if index * batch_size == n_read:
+        # #             loss = cae_model.train_on_batch(x=one_hot_encoded_reads[index * batch_size - 1::],
+        # #                                             y=[one_hot_encoded_reads[index * batch_size - 1::],
+        # #                                                predicted_clusters_tensor[index * batch_size - 1::]])
+        # #         else:
+        # #             loss = cae_model.train_on_batch(x=one_hot_encoded_reads[index * batch_size::],
+        # #                                             y=[one_hot_encoded_reads[index * batch_size::],
+        # #                                                predicted_clusters_tensor[index * batch_size::]])
+        # #         index = 0
+        # #     else:
+        # #         loss = cae_model.train_on_batch(x=one_hot_encoded_reads[index * batch_size:(index + 1) * batch_size],
+        # #                                         y=[one_hot_encoded_reads[index * batch_size:(index + 1) * batch_size],
+        # #                                            predicted_clusters_tensor[
+        # #                                            index * batch_size:(index + 1) * batch_size]])
+        # #         index += 1
 
-        dataset = tf.data.Dataset.from_tensor_slices(
-            (one_hot_encoded_reads, (one_hot_encoded_reads, predicted_clusters_tensor)))
-
-        dataset = dataset.shuffle(10).batch(config[data]['batch_size'])
-        cae_model.fit(dataset, epochs=1, verbose=config['verbose'])
+        # dataset = tf.data.Dataset.from_tensor_slices(
+        #     (one_hot_encoded_reads, (one_hot_encoded_reads, predicted_clusters_tensor)))
+        #
+        # dataset = dataset.shuffle(10).batch(config[data]['batch_size'])
+        # cae_model.fit(dataset, epochs=1, verbose=config['verbose'])
+        cae_model.fit(x=one_hot_encoded_reads, y=[one_hot_encoded_reads, predicted_clusters_tensor],
+                      batch_size=config[data]['batch_size'], epochs=1,
+                      verbose=config['verbose'])
 
     # TODO what is happening here?
     print('Final MEC after training:', best_mec_result)
-    _, _, cpr = performance.correct_phasing_rate(consensus_sequences)
+    _, min_indexes, cpr = performance.correct_phasing_rate(consensus_sequences)
     print('CRP after training:', cpr)
+    print('min_indexes after training:', min_indexes)
 
     # correction -> converge
-    old_mec_result = 0
-    new_mec_result = performance.minimum_error_correction(one_hot_encoded_reads, consensus_sequences)
+    # old_mec_result = 0
+    # new_mec_result = performance.minimum_error_correction(one_hot_encoded_reads, consensus_sequences)
+    #
+    # while old_mec_result != new_mec_result:
+    #     old_mec_result = new_mec_result
+    #     predicted_clusters = majority_voting.assign_reads_to_best_fitting_consensus_sequence(n_clusters,
+    #                                                                                          consensus_sequences,
+    #                                                                                          one_hot_encoded_reads)
+    #
+    #     consensus_sequences = majority_voting.align_reads_per_cluster(one_hot_encoded_reads,
+    #                                                                   predicted_clusters, n_clusters)
+    #     new_mec_result = performance.minimum_error_correction(one_hot_encoded_reads, consensus_sequences)
 
-    while old_mec_result != new_mec_result:
-        old_mec_result = new_mec_result
-        predicted_clusters = majority_voting.assign_reads_to_best_fitting_consensus_sequence(shape[0], n_clusters,
-                                                                                             consensus_sequences,
-                                                                                             one_hot_encoded_reads)
+    # min_sum, min_indexes, cpr = performance.correct_phasing_rate(consensus_sequences)
+    #
+    # print('final MEC:', new_mec_result)
+    # print('final CPR:', cpr)
+    # print('final min_indexes:', min_indexes)
+    # with open("CPR", "wb") as fp:
+    #     pickle.dump([min_sum, min_indexes, cpr], fp)
 
-        consensus_sequences = majority_voting.align_reads_per_cluster(one_hot_encoded_reads,
-                                                                      predicted_clusters, n_clusters)
-        new_mec_result = performance.minimum_error_correction(one_hot_encoded_reads, consensus_sequences)
-
-    min_sum, min_indexes, cpr = performance.correct_phasing_rate(consensus_sequences)
-    print('final MEC:', new_mec_result)
-    print('final CPR:', cpr)
-    with open("CPR", "wb") as fp:
-        pickle.dump([min_sum, min_indexes, cpr], fp)
-    # print(tf.squeeze(output_encoder, [0]).shape)
     print(f'{ColorCoding.OKGREEN}FINITO{ColorCoding.ENDC}')

majority_voting.py
@@ -3,13 +3,80 @@ import numpy as np
 import performance
 
 
+# old shape
+# def majority_vote_alignment(clustered_reads):
+#     clustered_reads_sum = clustered_reads.sum(axis=0)
+#     consensus_sequence = np.zeros((clustered_reads.shape[1], 4, 1), dtype=np.int)
+#     for i in range(clustered_reads_sum.shape[0]):
+#         if clustered_reads_sum[i].sum(axis=0) != 0:
+#             consensus_sequence[i, np.argmax(clustered_reads_sum[i])] = 1
+#         # TODO check what if multiple the same count?
+#     return consensus_sequence
+
+# Backup....
+# def majority_vote_alignment(clustered_reads):
+#     for i in range(len(clustered_reads)):
+#         clustered_reads[i][0][0] = 0
+#         clustered_reads[i][1][0] = 0
+#         clustered_reads[i][2][0] = 0
+#         clustered_reads[i][3][0] = 0
+#     clustered_reads_sum = clustered_reads.sum(axis=0)
+#
+#     consensus_sequence = np.zeros((4, clustered_reads.shape[2], 1), dtype=np.int)
+#
+#     indexes_not_covered_by_reads = np.where(np.sum(clustered_reads_sum, axis=0) == 0)[0]
+#
+#     print('indexes_not_covered_by_reads', indexes_not_covered_by_reads)
+#     max_clustered_read_indexes = np.argmax(clustered_reads_sum, axis=0).sum(axis=1)
+#     # print(max_clustered_read_indexes)
+#     for cluster_index in range(clustered_reads_sum.shape[0]):
+#         indexes = list(np.where(max_clustered_read_indexes == cluster_index))
+#         print('indexes', indexes)
+#
+#         if cluster_index == 0:
+#             indexes = np.array([i for i in indexes if i not in indexes_not_covered_by_reads])
+#         print('1', indexes)
+#         print('2', cluster_index)
+#         print(indexes)
+#         if len(indexes) != 0:
+#             consensus_sequence[cluster_index, indexes] = 1
+#     # print(consensus_sequence)
+#     # exit(-1)
+#     print('consensus_sequence', consensus_sequence)
+#     exit(-1)
+#     # TODO check what if multiple the same count?
+#     return consensus_sequence
+
 def majority_vote_alignment(clustered_reads):
     clustered_reads_sum = clustered_reads.sum(axis=0)
-    consensus_sequence = np.zeros((clustered_reads.shape[1], 4, 1), dtype=np.int)
-    for i in range(clustered_reads_sum.shape[0]):
-        if clustered_reads_sum[i].sum(axis=0) != 0:
-            consensus_sequence[i, np.argmax(clustered_reads_sum[i])] = 1
-        # TODO check what if multiple the same count?
+
+    consensus_sequence = np.zeros((4, clustered_reads.shape[2], 1), dtype=np.int)
+
+    indexes_not_covered_by_reads = np.where(np.sum(clustered_reads_sum, axis=0) == 0)[0]
+    max_clustered_read_indexes = np.argmax(clustered_reads_sum, axis=0).sum(axis=1)
+    # print(max_clustered_read_indexes)
+    for cluster_index in range(clustered_reads_sum.shape[0]):
+        indexes = list(np.where(max_clustered_read_indexes == cluster_index))
+        if cluster_index == 0:
+            indexes = np.array([i for i in indexes[0] if i not in indexes_not_covered_by_reads])
+        if len(indexes) != 0:
+            consensus_sequence[cluster_index, indexes] = 1
+
+        # for j in range(len(indexes_not_covered_by_reads)):
+        #     if len(np.where(
+        #             clustered_reads_sum[:, indexes_not_covered_by_reads[j]] == max(
+        #                 clustered_reads_sum[:, indexes_not_covered_by_reads[j]]))[
+        #                0]) != 1:  # if not covered, select the most dominant one based on 'ACGTcount'
+        #         tem = np.where(clustered_reads_sum[:, indexes_not_covered_by_reads[j]] == max(
+        #             clustered_reads_sum[:, indexes_not_covered_by_reads[j]]))[0]
+        #         consensus_sequence[cluster_index, indexes_not_covered_by_reads[j]] = tem[int(np.floor(
+        #             random.random() * len(tem)))] + 1
+        #     else:
+        #         consensus_sequence[cluster_index, indexes_not_covered_by_reads[j]] = np.argmax(
+        #             clustered_reads_sum[:, indexes_not_covered_by_reads[j]]) + 1
+
+    # print(consensus_sequence)
+    # exit(-1)
     return consensus_sequence
 
 
@@ -22,11 +89,31 @@ def align_reads_per_cluster(reads, predicted_clusters, n_clusters):
     return majority_consensus_sequences
 
 
-def assign_reads_to_best_fitting_consensus_sequence(n_reads, n_clusters, consensus_sequences, one_hot_encoded_reads):
-    predicted_clusters = np.zeros((n_reads, n_clusters), dtype=int)
-    for read_index in range(n_reads):
+def assign_reads_to_best_fitting_consensus_sequence(n_clusters, consensus_sequences, one_hot_encoded_reads):
+    # counter = 0
+    # for n in consensus_sequences:
+    #     print('consensus', counter)
+    #     counter += 1
+    #
+    #     for i in n:
+    #         line = []
+    #         for j in i:
+    #             line.append(j[0])
+    #         print(line)
+    # for i in one_hot_encoded_reads:
+    #     print('new read')
+    #     for e in i:
+    #         line = []
+    #         for j in e:
+    #             line.append(j[0])
+    #         print(line)
+    predicted_clusters = np.zeros((one_hot_encoded_reads.shape[0], n_clusters), dtype=int)
+    for read_index in range(one_hot_encoded_reads.shape[0]):
         hd = []
         for consensus_sequence in consensus_sequences:
             hd.append(performance.hamming_distance(one_hot_encoded_reads[read_index], consensus_sequence))
+
         predicted_clusters[read_index][np.argmin(hd)] = 1
+    # print(predicted_clusters)
+    # exit(-1)
     return predicted_clusters

models/autoencoder.py
@@ -1,3 +1,4 @@
+import numpy as np
 from tensorflow.python.keras import Sequential
 from tensorflow.python.keras.layers import InputLayer, Conv2D, PReLU, Dropout, Flatten, Dense, Reshape, Conv2DTranspose, \
     AvgPool2D, UpSampling2D
@@ -12,6 +13,7 @@ data = config['data']
 def get_autoencoder_key_points(input_shape):
     # input layer
     base_filter = 32
+    print(input_shape)
     model_input = Input(
         shape=(input_shape[0], input_shape[1], input_shape[2]), name='encoder_Input_0')
     # shape=(input_shape[0], input_shape[1], input_shape[2]), batch_size=batch_size, name='encoder_Input_0')
@@ -47,7 +49,7 @@ def get_autoencoder_key_points(input_shape):
     encoder_flatten_4 = Flatten(name='encoder_Flatten_4')(encoder_dropout_3)
     # 5. layer
     # TODO replace all others
-    latent_space = Dense(units=input_shape[0] / 4, name='encoder_Dense_5')(
+    latent_space = Dense(units=int(np.ceil(input_shape[0] / 4)), name='encoder_Dense_5')(
         encoder_flatten_4)
 
     decoder_dense_1 = Dense(units=base_filter * 4 * input_shape[0], name='decoder_Dense_1')(latent_space)
@@ -118,7 +120,7 @@ def get_autoencoder_key_points_with_pooling(input_shape):
     encoder_flatten_4 = Flatten(name='encoder_Flatten_4')(encoder_pooling_3)
     # 5. layer
     # TODO replace all others
-    latent_space = Dense(units=input_shape[0] / 4, name='encoder_Dense_5')(
+    latent_space = Dense(units=int(np.ceil(input_shape[0] / 4)), name='encoder_Dense_5')(
         encoder_flatten_4)
     # output_encoder = Dense(units=9852 / 4, name='encoder_Dense_5')(
     #     encoder_flatten_4)
@@ -154,82 +156,75 @@ def get_autoencoder_key_points_with_pooling(input_shape):
     return model_input, latent_space, decoder_output
 
 
-def get_autoencoder_key_points_with_pooling_with_relu(input_shape):
-    # input layer
-    base_filter = 16
+def get_CAECseq(shape):
+    drop_prob = 0
+
+    len_code = int(np.ceil(shape[1] / 4))
+    kernel = [5, 5, 3]
+    filters = [32, 64, 128]
     model_input = Input(
-        shape=(input_shape[0], input_shape[1], input_shape[2]), name='encoder_Input_0',
-    )
-    # shape=(input_shape[0], input_shape[1], input_shape[2]), batch_size=batch_size, name='encoder_Input_0')
+        shape=(shape[0], shape[1], shape[2]), name='model_input')
+    # convolutional layer - 1
+    conv_1 = Conv2D(filters=filters[0],
+                    kernel_size=[4, kernel[0]],
+                    strides=(4, 1),
+                    padding='same',
+                    name='conv_1')(model_input)
+    PRelu_1 = PReLU(name='PRelu_1')(conv_1)
+    drop_1 = Dropout(drop_prob, name='drop_1')(PRelu_1)
 
-    # 1. layer
-    encoder_conv2d_1 = Conv2D(
-        filters=base_filter,
-        kernel_size=[5, 4],
-        strides=(1, 4),
-        padding='same',
-        activation='relu',
-        name='encoder_Conv2D_1')(model_input)
-    encoder_dropout_1 = Dropout(0, name='encoder_Dropout_1')(encoder_conv2d_1)
+    conv_2 = Conv2D(filters=filters[1],
+                    kernel_size=[1, kernel[1]],
+                    strides=(1, 1),
+                    padding='same',
+                    name='conv_2')(drop_1)
+    PRelu_2 = PReLU(name='PRelu_2')(conv_2)
+    drop_2 = Dropout(drop_prob, name='drop_2')(PRelu_2)
 
-    # 2. layer
-    encoder_conv2d_2 = Conv2D(filters=base_filter * 2,
-                              kernel_size=[5, 1],
+    conv_3 = Conv2D(filters=filters[2],
+                    kernel_size=[1, kernel[2]],
+                    strides=(1, 1),
+                    padding='same',
+                    name='conv_3')(drop_2)
+    PRelu_3 = PReLU(name='PRelu_3')(conv_3)
+    drop_3 = Dropout(drop_prob, name='drop_3')(PRelu_3)
+    # flatten
+    flatten = Flatten(name='flatten')(drop_3)
+    # code
+    code = Dense(units=len_code,
+                 name='code')(flatten)
+    # dense
+    dense_1 = Dense(units=flatten.shape[1],
+                    name='dense_1')(code)
+    PRelu_4 = PReLU(name='PRelu_4')(dense_1)
+    # reshape
+    reshape = Reshape((drop_3.shape[1], drop_3.shape[2], drop_3.shape[3]),
+                      name='reshape')(PRelu_4)
+    # transposed convolution layer
+    convT_1 = Conv2DTranspose(filters=filters[1],
+                              kernel_size=[1, kernel[2]],
                               strides=(1, 1),
                               padding='same',
-                              activation='relu',
-                              name='encoder_Conv2D_2')(encoder_dropout_1)
-    encoder_pooling_2 = AvgPool2D((1, 1), strides=(2, 2))(encoder_conv2d_2)
-    encoder_dropout_2 = Dropout(0, name='encoder_Dropout_2')(encoder_pooling_2)
+                              name='convT_1')(reshape)
+    PRelu_5 = PReLU(name='PRelu_5')(convT_1)
+    drop_4 = Dropout(drop_prob, name='drop_4')(PRelu_5)
 
-    # 3. layer
-    encoder_conv2d_3 = Conv2D(filters=base_filter * 4,
-                              kernel_size=[3, 1],
+    convT_2 = Conv2DTranspose(filters=filters[0],
+                              kernel_size=[1, kernel[1]],
                               strides=(1, 1),
-                              activation='relu',
-                              padding='same', name='encoder_Conv2D_3')(encoder_dropout_2)
-    encoder_pooling_3 = AvgPool2D((1, 1), strides=(2, 2))(encoder_conv2d_3)
-    encoder_dropout_3 = Dropout(0, name='encoder_Dropout_3')(encoder_pooling_3)
+                              padding='same',
+                              name='convT_2')(drop_4)
+    PRelu_6 = PReLU(name='PRelu_6')(convT_2)
+    drop_5 = Dropout(drop_prob, name='drop_5')(PRelu_6)
 
-    # 4. layer
-    encoder_flatten_4 = Flatten(name='encoder_Flatten_4')(encoder_dropout_3)
-    # 5. layer
-    # TODO replace all others
-    output_encoder = Dense(units=input_shape[0] / 4, name='encoder_Dense_5')(
-        encoder_flatten_4)
-    # output_encoder = Dense(units=9852 / 4, name='encoder_Dense_5')(
-    #     encoder_flatten_4)
-
-    decoder_dense_1 = Dense(units=encoder_flatten_4.shape[1], name='decoder_Dense_1')(output_encoder)
-    decoder_prelu_2 = PReLU(name='decoder_PReLU_2')(decoder_dense_1)
-    decoder_reshape_2 = Reshape((encoder_dropout_3.shape[1], encoder_dropout_3.shape[2], encoder_dropout_3.shape[3]),
-                                activation='relu',
-                                name='decoder_Reshape_2')(decoder_prelu_2)
-    decoder_conv2dtranspose_3 = Conv2DTranspose(filters=base_filter * 2,
-                                                kernel_size=[3, 1],
-                                                strides=(1, 1),
-                                                padding='same',
-                                                activation='relu',
-                                                name='decoder_Conv2DTranspose_3')(decoder_reshape_2)
-    decoder_up_samling_3 = UpSampling2D((2, 1))(decoder_conv2dtranspose_3)
-    decoder_dropout_3 = Dropout(0, name='decoder_Dropout_3')(decoder_up_samling_3)
-    decoder_conv2dtranspose_4 = Conv2DTranspose(filters=base_filter,
-                                                kernel_size=[5, 1],
-                                                strides=(1, 1),
-                                                padding='same',
-                                                activation='relu',
-                                                name='decoder_Conv2DTranspose_4')(decoder_dropout_3)
-    decoder_up_samling_4 = UpSampling2D((2, 1))(decoder_conv2dtranspose_4)
-    decoder_dropout_4 = Dropout(0, name='decoder_Dropout_4')(decoder_up_samling_4)
-    decoder_conv2dtranspose_5 = Conv2DTranspose(filters=1,
-                                                kernel_size=[5, 4],
-                                                strides=(1, 4),
-                                                padding='same',
-                                                activation='relu',
-                                                name='decoder_Conv2DTranspose_5')(decoder_dropout_4)
-    decoder_output = Dropout(0, name='decoder_Dropout_5')(decoder_conv2dtranspose_5)
-
-    return model_input, output_encoder, decoder_output
+    convT_3 = Conv2DTranspose(filters=1,
+                              kernel_size=[4, kernel[0]],
+                              strides=(4, 1),
+                              padding='same',
+                              name='convT_3')(drop_5)
+    drop_6 = Dropout(drop_prob, name='drop_6')(convT_3)
+    return model_input \
+        , code, drop_6
 
 
 # class Dense(Layer):

one_hot_v3.py
@@ -87,81 +87,103 @@ def reverse_switcher(base):
     }.get(base, '-')
 
 
+# encrypt reads according to switcher
 def encode_read(position, cigar, read, length):
-    # adjust read according to CIGAR string
     adjusted_read = ''
-
-    if 'I' in cigar or 'D' in cigar or 'S' in cigar:
+    if 'I' in cigar or 'D' in cigar:
         cigar_position = 0
         cigar_list = [''.join(g) for _, g in groupby(cigar, str.isalpha)]
-        # delete insertions and add '-' for deletion
+        # delete insertions and add - for deletion
         for index in range(0, len(cigar_list), 2):
-            if cigar_list[index + 1] == 'S':
-                if int(cigar_list[index]) > 4:
-                    return
             if cigar_list[index + 1] == 'D':
                 adjusted_read += '-' * int(cigar_list[index])
             elif cigar_list[index + 1] != 'I':
                 adjusted_read += read[cigar_position:(cigar_position + int(cigar_list[index]))]
                 cigar_position += int(cigar_list[index])
-
     else:
         adjusted_read = read
 
-    # encode according to switcher
     switched_read = []
     for base in adjusted_read:
         switched_read.append(switcher(base))
     if sum(switched_read) == 0:
         return
-    # position read in list of length of haplotype
-    # embed read at its position, rest of sequence set to -1
+        # position read in list of length of haplotype
     positioned_read = [-1] * position
     positioned_read.extend(switched_read)
     positioned_read.extend(-1 for _ in range(length - len(switched_read) - position))
     # switched_read_tensor = tf.cast(positioned_read, tf.int32)
 
-    # if positioned_read[snps_index[10]] != -1:
-    #     print(positioned_read[snps_index[10]])
+    # if positioned_read[snps_index[1]] != -1:
+    #     print(positioned_read[snps_index[1]])
     # print(positioned_read[snps_index[1]])
-
-    # only take snp positions
     read_snps = [positioned_read[i] for i in snps_index]
-
-    # if positioned_read[snps_index[10]] != -1:
-    #     print('::::::::::::::::: snps_index[10]')
-    #     print('position', positioned_read[snps_index[10] - 2])
-    #     print('position', positioned_read[snps_index[10] - 1])
-    #     print('position', positioned_read[snps_index[10]])
-    #     print('position', positioned_read[snps_index[10] + 1])
-    #     print('position', positioned_read[snps_index[10] + 2])
-    #     # print('cigar', cigar)
-    #     # print('read', read)
-    #     # print('read', len(read))
-    #     print('adjusted_read', adjusted_read[snps_index[10]])
+    # if positioned_read[snps_index[0]] != -1 and positioned_read[snps_index[0]] != 2 and positioned_read[
+    #     snps_index[0]] != 0:
+    #     print('::::::::::::::::: snps_index[0]')
+    #     print('position', position)
+    #     print('cigar', cigar)
+    #     print('read', read)
+    #     print('read', len(read))
+    #     print('adjusted_read', adjusted_read)
     #     print('adjusted_read', len(adjusted_read))
     # else:
     #     print('----- CIGAR ------', cigar)
     #     print('read', len(read))
     #     print('adjusted_read', len(adjusted_read))
+    # print(read_snps)
 
-    # adjust length for to make pooling possible
     if len(read_snps) % 4 != 0:
         additional = 4 - len(read_snps) % 4
         for i in range(additional):
             read_snps.append(-1)
 
-    reconstructed_read = ''
-    # for i in read_snps:
-    #     reconstructed_read += reverse_switcher(i)
-    # print(reconstructed_read)
+    #         print(read_snps)
+    # TODO make length reasonable for
     switched_read_tensor = tf.cast(read_snps, tf.int32)
     # one_hot encode 0 -> [1,0,0,0], 1 -> [0,1,0,0] ... -1 -> [0,0,0,0]
     one_hot_snps = tf.one_hot(switched_read_tensor, depth=4, dtype=tf.int8)
-    # return one_hot_snps
-    transposed = tf.transpose(one_hot_snps)
+    return one_hot_snps
 
-    return transposed
+
+# def encode_read(position, read, length):
+#     switched_read = []
+#     for base in read:
+#         switched_read.append(switcher(base))
+#     # position read in list of length of haplotype
+#     positioned_read = [-1] * position
+#     positioned_read.extend(switched_read)
+#     positioned_read.extend(-1 for _ in range(length - len(switched_read) - position))
+#     # switched_read_tensor = tf.cast(positioned_read, tf.int32)
+#
+#     positioned_read_short = positioned_read[:length]
+#
+#     switched_read_tensor = tf.cast(positioned_read_short, tf.int32)
+#     # one_hot encode 0 -> [1,0,0,0], 1 -> [0,1,0,0] ... -1 -> [0,0,0,0]
+#     one_hot_read = tf.one_hot(switched_read_tensor, depth=4, dtype=tf.int8)
+#     return one_hot_read
+
+
+def encode_read_between(position, read, start, end):
+    switched_read = []
+    for base in read:
+        switched_read.append(switcher(base))
+    # position read in list of length of haplotype
+    positioned_read = []
+    if position > start:
+        positioned_read = [-1] * (position - start)
+
+    positioned_read.extend(switched_read)
+    if position + len(read) < end:
+        positioned_read.extend(-1 for _ in range(end - len(positioned_read)))
+    # switched_read_tensor = tf.cast(positioned_read, tf.int32)
+
+    positioned_read_short = positioned_read[:end - start]
+
+    switched_read_tensor = tf.cast(positioned_read_short, tf.int32)
+    # one_hot encode 0 -> [1,0,0,0], 1 -> [0,1,0,0] ... -1 -> [0,0,0,0]
+    one_hot_read = tf.one_hot(switched_read_tensor, depth=4, dtype=tf.int8)
+    return one_hot_read
 
 
 def decode_read(sequence):
@@ -178,6 +200,15 @@ def decode_read(sequence):
 
 def encode_sam():
     # check if file already exists
+    # if data == 'illumina':
+    #     if config['load'] and exists(config[data]['one_hot_path'] + '_0.npy'):
+    #         print('reads are already one hot encoded')
+    #         return
+    # # elif data == 454:
+    # #     if config['load'] and exists(config[data]['one_hot_path'] + '0.npy'):
+    # #         print('reads are already one hot encoded')
+    # #         return
+    # else:
     path = ''
     if data == 'created':
         path = '_' + str(config[data]['n_clusters'])
@@ -185,85 +216,161 @@ def encode_sam():
         print('reads are already one hot encoded')
         return
     # read reads
-    one_hot_encoded_snps = []
+    one_hot_encoded_reads = []
     print('reading file: encoding', config[data]['mapped_reads_path'])
     # read file line by line and one hot encode reads
+    # TODO do better more abstract
 
-    if data == 'created':
-        with open(config[data]['mapped_reads_path'] + '_' + str(config[data]['n_clusters']) + '.sam') as file:
-
-            counter = 0
-            for line in file:
-                # -1 because of sam numbering
-                pos = int(line.split('\t')[3]) - 1
-
-                one_hot_encoded_read_snps = encode_read(pos, line.split('\t')[5], line.split('\t')[9],
-                                                        config[data]['haplotype_length'])
-                if one_hot_encoded_read_snps is not None:
-                    one_hot_encoded_snps.append(one_hot_encoded_read_snps)
-                # print progress
-                counter += 1
-                if counter % 25000 == 0:
-                    print(counter)
-        one_hot_encoded_snp_tensor = tf.expand_dims(tf.convert_to_tensor(one_hot_encoded_snps), axis=3)
-        # save
-        save_tensor_file(config[data]['one_hot_path'] + '_' + str(config[data]['n_clusters']),
-                         one_hot_encoded_snp_tensor)
-        print('Reads are one hot encoded and saved.')
-
-    else:
+    if data == 454 or data == 'illumina':
         with open(config[data]['mapped_reads_path'] + '.sam') as file:
             counter = 0
             for line in file:
-                # -1 because of sam numbering
+                # if int(line.split('\t')[3]) > config[data]['haplotype_length']:
+                #     continue
+                counter += 1
                 pos = int(line.split('\t')[3]) - 1
-                one_hot_encoded_read_snps = encode_read(pos, line.split('\t')[5], line.split('\t')[9],
-                                                        config[data]['haplotype_length'])
+                one_hot_encoded_read = encode_read(pos, line.split('\t')[5], line.split('\t')[9],
+                                                   config[data]['haplotype_length'])
+                if one_hot_encoded_read is not None:
+                    one_hot_encoded_reads.append(one_hot_encoded_read)
+                if counter % 25000 == 0:
+                    print(counter)
+        one_hot_encoded_reads_tensor = tf.expand_dims(tf.convert_to_tensor(one_hot_encoded_reads), axis=3)
+        # save
+        save_tensor_file(config[data]['one_hot_path'], one_hot_encoded_reads_tensor)
+        print('Reads are one hot encoded and saved.')
 
-                if one_hot_encoded_read_snps is not None:
-                    one_hot_encoded_snps.append(one_hot_encoded_read_snps)
-
-                # print progress
+    elif data == 'per_gene_454':
+        start = config[data]['haplotype_start']
+        end = config[data]['haplotype_end']
+        # length must be dividable by 4 to work with pooling and upsampling
+        end += (4 - (end - start) % 4)
+        with open(config[data]['mapped_reads_path'] + '.sam') as file:
+            counter = 0
+            for line in file:
+                pos = int(line.split('\t')[3])
+                length = len(line.split('\t')[9])
+                if pos > end or (pos + length) < start:
+                    continue
+                counter += 1
+                one_hot_encoded_read = encode_read_between(pos, line.split('\t')[9], start, end)
+                one_hot_encoded_reads.append(one_hot_encoded_read)
                 counter += 1
                 if counter % 25000 == 0:
                     print(counter)
-        one_hot_encoded_snp_tensor = tf.expand_dims(tf.convert_to_tensor(one_hot_encoded_snps), axis=3)
+        print(one_hot_encoded_reads)
+
+        one_hot_encoded_reads_tensor = tf.expand_dims(tf.convert_to_tensor(one_hot_encoded_reads), axis=3)
+        # save
+        save_tensor_file(config[data]['one_hot_path'], one_hot_encoded_reads_tensor)
+        print('Reads are one hot encoded and saved.')
+
+
+    elif data == 'created':
+        with open(config[data]['mapped_reads_path'] + '_' + str(config[data]['n_clusters']) + '.sam') as file:
+            counter = 0
+            for line in file:
+                pos = int(line.split('\t')[3]) - 1
+
+                one_hot_encoded_read = encode_read(pos, line.split('\t')[5], line.split('\t')[9],
+                                                   config[data]['haplotype_length'])
+                one_hot_encoded_reads.append(one_hot_encoded_read)
+                counter += 1
+                if counter % 25000 == 0:
+                    print(counter)
+        # if len(one_hot_encoded_reads) % 4 != 0:
+        #     empty_read = encode_read(0, '', '', config[data]['haplotype_length'])
+        #     for i in range(len(one_hot_encoded_reads) % 4):
+        #         one_hot_encoded_reads.append(empty_read)
+        one_hot_encoded_reads_tensor = tf.expand_dims(tf.convert_to_tensor(one_hot_encoded_reads), axis=3)
 
         # save
-        save_tensor_file(config[data]['one_hot_path'], one_hot_encoded_snp_tensor)
+        save_tensor_file(config[data]['one_hot_path'] + '_' + str(config[data]['n_clusters']),
+                         one_hot_encoded_reads_tensor)
         print('Reads are one hot encoded and saved.')
+
+    # elif data == 454:
+    #     with open(config[data]['mapped_reads_path']) as file:
+    #         counter = 0
+    #         file_index = 0
+    #         for line in file:
+    #             one_hot_encoded_read = encode_read(int(line.split('\t')[3]), line.split('\t')[9],
+    #                                                config[data]['haplotype_length'])
+    #             one_hot_encoded_reads.append(one_hot_encoded_read)
+    #             counter += 1
+    #             if counter % 25000 == 0:
+    #                 print(counter)
+    #             if counter % config[data]['batch_size'] == 0:
+    #                 one_hot_encoded_reads_tensor = tf.expand_dims(tf.convert_to_tensor(one_hot_encoded_reads),
+    #                                                               axis=3)
+    #                 # save
+    #                 print('Reads are one hot encoded and saved. file index:', file_index)
+    #
+    #                 save_tensor_file(config[data]['one_hot_path'] + str(file_index),
+    #                                  one_hot_encoded_reads_tensor)
+    #                 file_index += 1
+    #                 one_hot_encoded_reads = []
+    #     one_hot_encoded_reads_tensor = tf.expand_dims(tf.convert_to_tensor(one_hot_encoded_reads), axis=3)
+    #
+    #     # save
+    #
+    #     save_tensor_file(config[data]['one_hot_path'] + str(file_index), one_hot_encoded_reads_tensor)
+    #     print('Reads are one hot encoded and saved.')
+    else:
+        file_index = 0
+        with open(config[data]['mapped_reads_path'] + '.sam') as file:
+            counter = 0
+            for line in file:
+                one_hot_encoded_read = encode_read(int(line.split('\t')[3]), line.split('\t')[9],
+                                                   config[data]['haplotype_length'])
+                one_hot_encoded_reads.append(one_hot_encoded_read)
+                counter += 1
+                if counter % 25000 == 0:
+                    print(counter)
+                    if counter % 200000 == 0:
+                        one_hot_encoded_reads_tensor = tf.expand_dims(tf.convert_to_tensor(one_hot_encoded_reads),
+                                                                      axis=3)
+                        # save
+                        print('Reads are one hot encoded and saved. file index:', file_index)
+
+                        save_tensor_file(config[data]['one_hot_path'] + '_' + str(file_index),
+                                         one_hot_encoded_reads_tensor)
+                        file_index += 1
+                        one_hot_encoded_reads = []
+        one_hot_encoded_reads_tensor = tf.expand_dims(tf.convert_to_tensor(one_hot_encoded_reads),
+                                                      axis=3)
+        # save
+        save_tensor_file(config[data]['one_hot_path'] + '_' + str(file_index),
+                         one_hot_encoded_reads_tensor)
+        print('Reads are one hot encoded and saved. file index:', file_index)
+        one_hot_encoded_reads = []
+    # return file_index
 
 
 def encode_fasta():
     one_hot_encoded_sequences = []
+    names_of_variants = []
     print('reading file: REF.fasta')
-    # read file line by line and one hot encode sequences
-    counter = 0
+    # read file line by line and one hot encode reads
     if data == 'created':
         with open(config[data]['aligned_ref_path'] + '_' + str(config[data]['n_clusters']) + '.fasta') as file:
             for line in file:
                 if line[0] != '>':
-                    one_hot_encoded_sequence = encode_read(0, '', line[:-1],
+                    one_hot_encoded_sequence = encode_read(0, '', line,
                                                            config[data]['haplotype_length'])
                     one_hot_encoded_sequences.append(one_hot_encoded_sequence)
-        one_hot_encoded_reads_tensor = tf.expand_dims(tf.convert_to_tensor(one_hot_encoded_sequences), axis=3)
-
-        # save
-        save_tensor_file(config[data]['aligned_ref_path'] + '_' + str(config[data]['n_clusters']),
-                         one_hot_encoded_reads_tensor)
     else:
         with open(config[data]['aligned_ref_path'] + '.fasta') as file:
             for line in file:
                 if line[0] != '>':
-                    one_hot_encoded_sequence = encode_read(0, '', line[:-1],
+                    one_hot_encoded_sequence = encode_read(0, '', line,
                                                            config[data]['haplotype_length'])
                     one_hot_encoded_sequences.append(one_hot_encoded_sequence)
+    one_hot_encoded_reads_tensor = tf.expand_dims(tf.convert_to_tensor(one_hot_encoded_sequences), axis=3)
 
-        one_hot_encoded_reads_tensor = tf.expand_dims(tf.convert_to_tensor(one_hot_encoded_sequences), axis=3)
-
-        # save
-        save_tensor_file(config[data]['aligned_ref_path'],
-                         one_hot_encoded_reads_tensor)
+    # save
+    save_tensor_file(config[data]['aligned_ref_path'] + '_' + str(config[data]['n_clusters']),
+                     one_hot_encoded_reads_tensor)
     print('Reads are one hot encoded and saved.')
 
 
@@ -272,7 +379,7 @@ def decode(encoded_sequences, ref=False):
     for i in range(len(encoded_sequences)):
         decoded_sequences.append('>' + str(i))
         decoded_sequences.append(decode_read(encoded_sequences[i]))
-        # print(decoded_sequences[2 * i], decoded_sequences[2 * i + 1])
+        print(decoded_sequences[2 * i], decoded_sequences[2 * i + 1])
     decoded_sequences_string = '\n'.join(decoded_sequences)
     if data == 'created':
         if ref:

performance.py
@@ -10,10 +10,15 @@ config = get_config()
 data = config['data']
 
 
+# old shape
+# def hamming_distance(read, consensus_sequence):
+#     index_of_non_zero = ([np.where(read != 0)][0][0])
+#     difference = read[index_of_non_zero] - consensus_sequence[index_of_non_zero]
+#     return np.count_nonzero(difference) / 2
+
 def hamming_distance(read, consensus_sequence):
-    index_of_non_zero = ([np.where(read != 0)][0][0])
-    difference = read[index_of_non_zero] - consensus_sequence[index_of_non_zero]
-    return np.count_nonzero(difference) / 2
+    difference = (consensus_sequence - read)[np.where(read != 0)]
+    return np.count_nonzero(difference)
 
 
 def minimum_error_correction(reads, consensus_sequences):
@@ -26,6 +31,74 @@ def minimum_error_correction(reads, consensus_sequences):
     return mec
 
 
+# # old shape
+# def correct_phasing_rate(consensus_sequences):
+#     if data == 'created':
+#         reference_sequences = load_tensor_file(
+#             config[data]['aligned_ref_path'] + '_' + str(config[data]['n_clusters']))
+#     else:
+#         reference_sequences = load_tensor_file(config[data]['aligned_ref_path'])
+#
+#     print('\nreference_sequences:\n', one_hot.decode(reference_sequences, True))
+#
+#     print('\nconsensus_sequences:\n', one_hot.decode(consensus_sequences))
+#
+#     if len(consensus_sequences) != len(reference_sequences):
+#         print(
+#             'number of consensus sequences and reference sequences mismatch, correction phasing rate can\'t be '
+#             'calculated')
+#         return
+#     distances = np.zeros((len(consensus_sequences), len(reference_sequences)))
+#     if data == 'per_gene_454':
+#         # TODO do per consensus sequence
+#         start = config[data]['haplotype_start']
+#         for cs_index, consensus_sequence in enumerate(consensus_sequences):
+#             for ref_index, reference_sequence in enumerate(reference_sequences):
+#                 hd_per_ref = []
+#                 # window of index plus minus 100
+#                 for start_index in range(start - 100, start + 100):
+#                     end_index = start_index + len(consensus_sequence)
+#                     hd_per_ref.append(
+#                         hamming_distance(consensus_sequence, reference_sequence[start_index:end_index]))
+#                     # reference_sequence = np.roll(reference_sequence, 1, axis=0)
+#                 distances[cs_index][ref_index] = min(hd_per_ref)
+#
+#     else:
+#         for cs_index, consensus_sequence in enumerate(consensus_sequences):
+#             for ref_index, reference_sequence in enumerate(reference_sequences):
+#                 hd_per_ref = []
+#                 # if last element not part of sequence
+#                 # -> move reference sequence to the right till last element is part of sequence
+#                 if np.sum(reference_sequence[-1]) != 0:
+#                     hd_per_ref.append(hamming_distance(consensus_sequence, reference_sequence))
+#                 else:
+#                     for index in range(len(reference_sequence - 1), -1, -1):
+#                         if np.sum(reference_sequence[-1]) != 0:
+#                             break
+#                         hd_per_ref.append(hamming_distance(consensus_sequence, reference_sequence))
+#                         reference_sequence = np.roll(reference_sequence, 1, axis=0)
+#
+#                 distances[cs_index][ref_index] = min(hd_per_ref)
+#
+#     min_indexes = []
+#     min_sum = 0
+#     indexes = list(range(len(reference_sequences)))
+#     permutations = list(itertools.permutations(indexes))
+#     for permutation in permutations:
+#         tmp_sum = 0
+#         for i in range(len(consensus_sequences)):
+#             tmp_sum += distances[i][permutation[i]]
+#         if len(min_indexes) == 0 or tmp_sum < min_sum:
+#             min_indexes = permutation
+#             min_sum = tmp_sum
+#
+#     # print('min_sum', min_sum)
+#     cpr = 1 - (min_sum / (len(consensus_sequences) * reference_sequences.shape[1]))
+#     # print(cpr)
+#
+#     return min_sum, min_indexes, cpr
+
+
 def correct_phasing_rate(consensus_sequences):
     if data == 'created':
         reference_sequences = load_tensor_file(
@@ -33,8 +106,11 @@ def correct_phasing_rate(consensus_sequences):
     else:
         reference_sequences = load_tensor_file(config[data]['aligned_ref_path'])
 
+    reference_sequences = np.transpose(reference_sequences, axes=(0, 2, 1, 3))
+    consensus_sequences = np.transpose(consensus_sequences, axes=(0, 2, 1, 3))
+
+    print('\nreference_sequences:\n', one_hot.decode(reference_sequences, True))
     print('\nconsensus_sequences:\n', one_hot.decode(consensus_sequences))
-    print('\nreference_sequences:\n', one_hot.decode(reference_sequences))
 
     if len(consensus_sequences) != len(reference_sequences):
         print(
@@ -42,36 +118,21 @@ def correct_phasing_rate(consensus_sequences):
             'calculated')
         return
     distances = np.zeros((len(consensus_sequences), len(reference_sequences)))
-    if data == 'per_gene_454':
-        # TODO do per consensus sequence
-        start = config[data]['haplotype_start']
-        for cs_index, consensus_sequence in enumerate(consensus_sequences):
-            for ref_index, reference_sequence in enumerate(reference_sequences):
-                hd_per_ref = []
-                # window of index plus minus 100
-                for start_index in range(start - 100, start + 100):
-                    end_index = start_index + len(consensus_sequence)
-                    hd_per_ref.append(
-                        hamming_distance(consensus_sequence, reference_sequence[start_index:end_index]))
-                    # reference_sequence = np.roll(reference_sequence, 1, axis=0)
-                distances[cs_index][ref_index] = min(hd_per_ref)
-
-    else:
-        for cs_index, consensus_sequence in enumerate(consensus_sequences):
-            for ref_index, reference_sequence in enumerate(reference_sequences):
-                hd_per_ref = []
-                # if last element not part of sequence
-                # -> move reference sequence to the right till last element is part of sequence
-                if np.sum(reference_sequence[-1]) != 0:
+    for cs_index, consensus_sequence in enumerate(consensus_sequences):
+        for ref_index, reference_sequence in enumerate(reference_sequences):
+            hd_per_ref = []
+            # if last element not part of sequence
+            # -> move reference sequence to the right till last element is part of sequence
+            if np.sum(reference_sequence[-1]) != 0:
+                hd_per_ref.append(hamming_distance(consensus_sequence, reference_sequence))
+            else:
+                for index in range(len(reference_sequence - 1), -1, -1):
+                    if np.sum(reference_sequence[-1]) != 0:
+                        break
                     hd_per_ref.append(hamming_distance(consensus_sequence, reference_sequence))
-                else:
-                    for index in range(len(reference_sequence - 1), -1, -1):
-                        if np.sum(reference_sequence[-1]) != 0:
-                            break
-                        hd_per_ref.append(hamming_distance(consensus_sequence, reference_sequence))
-                        reference_sequence = np.roll(reference_sequence, 1, axis=0)
+                    reference_sequence = np.roll(reference_sequence, 1, axis=0)
 
-                distances[cs_index][ref_index] = min(hd_per_ref)
+            distances[cs_index][ref_index] = min(hd_per_ref)
 
     min_indexes = []
     min_sum = 0
@@ -85,8 +146,9 @@ def correct_phasing_rate(consensus_sequences):
             min_indexes = permutation
             min_sum = tmp_sum
 
-    # print('min_sum', min_sum)
+    print('min_sum', min_sum)
     print(reference_sequences.shape[1])
+    print(len(consensus_sequences))
     cpr = 1 - (min_sum / (len(consensus_sequences) * reference_sequences.shape[1]))
     # print(cpr)
     return min_sum, min_indexes, cpr